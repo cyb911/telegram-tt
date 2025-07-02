56609e718e4007ac0f5fb7c4820a981f0634da60
yunbin.chen <yunbin110@gmail.com>
2025-07-02 13:06:54 +0800

    删除冗余code

--- src/components/common/helpers/renderMessageText.ts
+++ src/components/common/helpers/renderMessageText.ts
@@ -1,23 +1,11 @@
-import { getGlobal } from '../../../global';
-
 import type { ApiMessage, ApiSponsoredMessage } from '../../../api/types';
-import type { OldLangFn } from '../../../hooks/useOldLang';
-import type { TextPart, ThreadId } from '../../../types';
-import { ApiMessageEntityTypes } from '../../../api/types';
+import type { ThreadId } from '../../../types';
 
 import {
-  getMessageStatefulContent,
   getMessageText,
 } from '../../../global/helpers';
-import {
-  getMessageSummaryDescription,
-  getMessageSummaryEmoji,
-  getMessageSummaryText,
-  TRUNCATED_SUMMARY_LENGTH,
-} from '../../../global/helpers/messageSummary';
 import { getMessageKey } from '../../../util/keys/messageKey';
 import trimText from '../../../util/trimText';
-import renderText from './renderText';
 import { renderTextWithEntities } from './renderTextWithEntities';
 
 export function renderMessageText({
@@ -69,43 +57,4 @@ export function renderMessageText({
     threadId,
     maxTimestamp,
   });
-}
-
-// TODO Use Message Summary component instead
-export function renderMessageSummary(
-  lang: OldLangFn,
-  message: ApiMessage,
-  noEmoji = false,
-  highlight?: string,
-  truncateLength = TRUNCATED_SUMMARY_LENGTH,
-): TextPart[] {
-  const { entities } = message.content.text || {};
-
-  const global = getGlobal();
-  const statefulContent = getMessageStatefulContent(global, message);
-
-  const hasSpoilers = entities?.some((e) => e.type === ApiMessageEntityTypes.Spoiler);
-  const hasCustomEmoji = entities?.some((e) => e.type === ApiMessageEntityTypes.CustomEmoji);
-  if (!hasSpoilers && !hasCustomEmoji) {
-    const text = trimText(getMessageSummaryText(lang, message, statefulContent, noEmoji), truncateLength);
-
-    if (highlight) {
-      return renderText(text, ['emoji', 'highlight'], { highlight });
-    } else {
-      return renderText(text);
-    }
-  }
-
-  const emoji = !noEmoji && getMessageSummaryEmoji(message);
-  const emojiWithSpace = emoji ? `${emoji} ` : '';
-
-  const text = renderMessageText({
-    message, highlight, asPreview: true, truncateLength,
-  });
-  const description = getMessageSummaryDescription(lang, message, statefulContent, text);
-
-  return [
-    ...renderText(emojiWithSpace),
-    ...(Array.isArray(description) ? description : [description]),
-  ].filter(Boolean);
 }
