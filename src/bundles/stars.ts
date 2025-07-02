77a5d7757c950dacde930bbfb3c8e477231c615e
yunbin.chen <yunbin110@gmail.com>
2025-07-02 08:13:29 +0800

    删除

--- src/bundles/stars.ts
+++ src/bundles/stars.ts
@@ -4,7 +4,6 @@ export { default as StarsBalanceModal } from '../components/modals/stars/StarsBa
 export { default as StarPaymentModal } from '../components/modals/stars/StarsPaymentModal';
 export { default as StarsTransactionInfoModal } from '../components/modals/stars/transaction/StarsTransactionModal';
 export { default as StarsSubscriptionModal } from '../components/modals/stars/subscription/StarsSubscriptionModal';
-export { default as PaidReactionModal } from '../components/modals/paidReaction/PaidReactionModal';
 export { default as GiftModal } from '../components/modals/gift/GiftModal';
 export { default as GiftRecipientPicker } from '../components/modals/gift/recipient/GiftRecipientPicker';
 export { default as GiftInfoModal } from '../components/modals/gift/info/GiftInfoModal';
