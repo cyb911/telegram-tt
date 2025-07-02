eef3290fd748ac368d80058730a8bde4562eca59
yunbin.chen <yunbin110@gmail.com>
2025-07-02 08:38:35 +0800

    删除

--- src/bundles/stars.ts
+++ src/bundles/stars.ts
@@ -1,9 +1,6 @@
 export { default as StarsGiftingPickerModal } from '../components/main/premium/StarsGiftingPickerModal';
-export { default as GiftModal } from '../components/modals/gift/GiftModal';
 export { default as GiftRecipientPicker } from '../components/modals/gift/recipient/GiftRecipientPicker';
-export { default as GiftInfoModal } from '../components/modals/gift/info/GiftInfoModal';
 export { default as GiftResalePriceComposerModal } from '../components/modals/gift/resale/GiftResalePriceComposerModal';
-export { default as GiftUpgradeModal } from '../components/modals/gift/upgrade/GiftUpgradeModal';
 export { default as GiftStatusInfoModal } from '../components/modals/gift/status/GiftStatusInfoModal';
 export { default as GiftWithdrawModal } from '../components/modals/gift/withdraw/GiftWithdrawModal';
 export { default as GiftTransferModal } from '../components/modals/gift/transfer/GiftTransferModal';
