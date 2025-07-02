84ceff2778c21b27052139a2ea0845861d7a2fb8
yunbin.chen <yunbin110@gmail.com>
2025-07-02 11:38:38 +0800

    删除冗余code

--- src/bundles/extra.ts
+++ src/bundles/extra.ts
@@ -2,7 +2,6 @@ export { default as WebAppModal } from '../components/modals/webApp/WebAppModal'
 export { default as PremiumMainModal } from '../components/main/premium/PremiumMainModal';
 export { default as GiveawayModal } from '../components/main/premium/GiveawayModal';
 export { default as PremiumLimitReachedModal } from '../components/main/premium/common/PremiumLimitReachedModal';
-export { default as BoostModal } from '../components/modals/boost/BoostModal';
 
 export { default as LocationAccessModal } from '../components/modals/locationAccess/LocationAccessModal';
 export { default as CalendarModal } from '../components/common/CalendarModal';
