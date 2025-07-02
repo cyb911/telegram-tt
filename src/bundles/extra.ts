0eba51c3fa98130a1d4b853fef4f51a435c76894
yunbin.chen <yunbin110@gmail.com>
2025-07-03 00:01:39 +0800

    文案

--- src/bundles/extra.ts
+++ src/bundles/extra.ts
@@ -9,7 +9,6 @@ export { default as SeenByModal } from '../components/common/SeenByModal';
 export { default as PrivacySettingsNoticeModal } from '../components/common/PrivacySettingsNoticeModal';
 export { default as ReactorListModal } from '../components/middle/ReactorListModal';
 export { default as EmojiInteractionAnimation } from '../components/middle/EmojiInteractionAnimation';
-export { default as ChatLanguageModal } from '../components/middle/ChatLanguageModal';
 
 export { default as CustomEmojiSetsModal } from '../components/common/CustomEmojiSetsModal';
 
