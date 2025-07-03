21ec9011c4f4d48b9f5052bf7976ad18ac1d0860
yunbin.chen <yunbin110@gmail.com>
2025-07-03 14:26:30 +0800

    删除冗余代码

--- src/bundles/extra.ts
+++ src/bundles/extra.ts
@@ -1,11 +1,8 @@
-export { default as WebAppModal } from '../components/modals/webApp/WebAppModal';
 export { default as PremiumMainModal } from '../components/main/premium/PremiumMainModal';
 export { default as GiveawayModal } from '../components/main/premium/GiveawayModal';
 export { default as PremiumLimitReachedModal } from '../components/main/premium/common/PremiumLimitReachedModal';
 
-export { default as LocationAccessModal } from '../components/modals/locationAccess/LocationAccessModal';
 export { default as CalendarModal } from '../components/common/CalendarModal';
-export { default as SeenByModal } from '../components/common/SeenByModal';
 
 export { default as CustomEmojiSetsModal } from '../components/common/CustomEmojiSetsModal';
 
