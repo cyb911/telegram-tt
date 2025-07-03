b95261f35ba5e65996f7876d94fa701099c7621b
yunbin.chen <yunbin110@gmail.com>
2025-07-03 10:21:32 +0800

    删除冗余代码

--- src/hooks/useFolderManager.ts
+++ src/hooks/useFolderManager.ts
@@ -1,20 +1,10 @@
 import { useEffect } from '../lib/teact/teact';
 
 import {
-  addOrderedIdsCallback,
   addUnreadCountersCallback,
-  getOrderedIds,
   getUnreadCounters,
 } from '../util/folderManager';
 import useForceUpdate from './useForceUpdate';
-
-export function useFolderManagerForOrderedIds(folderId: number) {
-  const forceUpdate = useForceUpdate();
-
-  useEffect(() => addOrderedIdsCallback(folderId, forceUpdate), [folderId, forceUpdate]);
-
-  return getOrderedIds(folderId);
-}
 
 export function useFolderManagerForUnreadCounters() {
   const forceUpdate = useForceUpdate();
