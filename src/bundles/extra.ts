c0632ff8a1d40e7836671196353566b11da3aee5
yunbin.chen <yunbin110@gmail.com>
2025-07-05 23:50:15 +0800

    删除冗余代码

--- src/bundles/extra.ts
+++ src/bundles/extra.ts
@@ -1,5 +1,4 @@
 export { default as PremiumMainModal } from '../components/main/premium/PremiumMainModal';
-export { default as GiveawayModal } from '../components/main/premium/GiveawayModal';
 export { default as PremiumLimitReachedModal } from '../components/main/premium/common/PremiumLimitReachedModal';
 
 export { default as CalendarModal } from '../components/common/CalendarModal';
